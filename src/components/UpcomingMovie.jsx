35153cee1b12f061cfa2730558bb95e5737b0534
rosanno <rosannomacabuhay@gmail.com>
2023-06-14 16:13:21 +0800

    Improve design

--- src/components/UpcomingMovie.jsx
+++ src/components/UpcomingMovie.jsx
@@ -1,38 +1,38 @@
-import React from 'react'
-import { useGetUpComingMovieQuery } from '../services/api';
+import React from "react";
+import { useGetUpComingMovieQuery } from "../services/api";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function UpcomingMovie() {
-
-    const {data} = useGetUpComingMovieQuery();
-   console.log(data);
+  const { data } = useGetUpComingMovieQuery();
+  console.log(data);
 
   return (
-
-
-  <section className='rounded'>
-<h1 className='text-xl font-mono mb-5'>Upcoming Movies</h1>
-<div className='grid grid-rows-2 grid-flow-col gap-1 overflow-x-scroll overflow-y-hidden h-screen'>
-
-{data?.results?.map(item=>(
- 
-<div key={item?.id} className='w-80 h-80 pe-20' >
-<Link to={`/movies/details/${item.id}`}>
-        <img src={`https://image.tmdb.org/t/p/w500${item?.poster_path}`} alt="" className='w-64 max-h-56 rounded' />
-<p>{item?.title}</p>
-<span className='flex justify-between'><li>{item.original_language}</li>
-<li>{item?.release_date}</li></span>
-</Link>
-   </div>
-))}
-
-
-
-</div>
-  
-   </section>
-  )
+    <section className="md:pl-6 md:pt-14  lg:ml-28">
+      <h1 className="text-xl mb-5 font-bold capitalize">Upcoming Movies</h1>
+      <div className="grid grid-rows-2 grid-flow-col gap-7 overflow-x-scroll overflow-y-hidden scrollbar">
+        {data?.results?.map((item) => (
+          <Link to={`/movies/details/${item.id}`} key={item.id}>
+            <div className="relative group overflow-hidden">
+              <div className="w-96 rounded-md overflow-hidden">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${item?.backdrop_path}`}
+                  alt=""
+                  className="rounded-md h-[270px] object-cover group-hover:scale-110 overflow-hidden transition-transform duration-300"
+                />
+              </div>
+              <div className="absolute bottom-10 z-10 px-5">
+                <h2 className="text-lg font-bold">{item?.title}</h2>
+                <span>{item.original_language}</span>
+                <span>{item?.release_date}</span>
+              </div>
+              <div className="absolute top-0 w-full h-full bg-black/30 rounded-md overflow-hidden" />
+            </div>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default UpcomingMovie
+export default UpcomingMovie;
