fd766c065d6d2fe01d68f94e85ae2825ca0cb92f
rosanno <rosannomacabuhay@gmail.com>
2023-06-14 16:58:46 +0800

    Completed UpcomingMovie section

--- src/components/UpcomingMovie.jsx
+++ src/components/UpcomingMovie.jsx
@@ -2,19 +2,22 @@ import React from "react";
 import { useGetUpComingMovieQuery } from "../services/api";
 
 import { Link } from "react-router-dom";
+import moment from "moment";
 
 function UpcomingMovie() {
   const { data } = useGetUpComingMovieQuery();
   console.log(data);
 
   return (
-    <section className="md:pl-6 md:pt-14  lg:ml-28">
-      <h1 className="text-xl mb-5 font-bold capitalize">Upcoming Movies</h1>
-      <div className="grid grid-rows-2 grid-flow-col gap-7 overflow-x-scroll overflow-y-hidden scrollbar">
+    <section className="md:pl-6 px-2 md:pt-14 lg:ml-28">
+      <h1 className="text-lg sm:text-3xl mb-5 font-bold capitalize">
+        Upcoming Movies
+      </h1>
+      <div className="grid grid-rows-2 grid-flow-col gap-5 sm:gap-7 md:mt-7 overflow-x-scroll overflow-y-hidden scrollbar">
         {data?.results?.map((item) => (
           <Link to={`/movies/details/${item.id}`} key={item.id}>
             <div className="relative group overflow-hidden">
-              <div className="w-96 rounded-md overflow-hidden">
+              <div className="w-72 sm:w-96 rounded-md overflow-hidden">
                 <img
                   src={`https://image.tmdb.org/t/p/w500${item?.backdrop_path}`}
                   alt=""
@@ -23,10 +26,15 @@ function UpcomingMovie() {
               </div>
               <div className="absolute bottom-10 z-10 px-5">
                 <h2 className="text-lg font-bold">{item?.title}</h2>
-                <span>{item.original_language}</span>
-                <span>{item?.release_date}</span>
+                <span className="text-gray-300">
+                  {item.original_language}
+                </span>{" "}
+                •{" "}
+                <span className="text-sm text-gray-300">
+                  {moment(item?.release_date).format("MMMM D YYYY")}
+                </span>
               </div>
-              <div className="absolute top-0 w-full h-full bg-black/30 rounded-md overflow-hidden" />
+              <div className="absolute top-0 w-full h-full bg-black/40 rounded-md overflow-hidden" />
             </div>
           </Link>
         ))}
