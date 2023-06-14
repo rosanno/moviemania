7aa58e13f66935fbef9e327097627489e3a9366b
rosanno <rosannomacabuhay@gmail.com>
2023-06-14 13:42:22 +0800

    Updated home pages and Popular Movies pages

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,15 @@
 import Hero from "../components/Hero";
 import Loader from "../components/Loader/Loader";
+import NowShowing from "../components/NowShowing";
 import Showcase from "../components/Showcase";
 import Content from "../components/content/Content";
 import { useGetRandomMovie } from "../hooks/useGetRandomMovie";
-import { useGetTrendingQuery } from "../services/api";
+import { useGetPopularSeriesQuery, useGetTrendingQuery } from "../services/api";
 
 const Home = () => {
   const { data, isLoading } = useGetTrendingQuery({ type: "movies" });
   const { randomMovie } = useGetRandomMovie(data);
+  const { data: popularSeries } = useGetPopularSeriesQuery();
 
   return (
     <>
@@ -18,6 +20,9 @@ const Home = () => {
         ) : (
           <Showcase media={randomMovie} isMediaSelected={false} />
         )}
+      </Content>
+      <Content isSpacerOnly>
+        <NowShowing />
       </Content>
     </>
   );
