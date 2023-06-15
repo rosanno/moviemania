9a84dd0389ee9ee090af7a1bb73b7403da512a81
rosanno <rosannomacabuhay@gmail.com>
2023-06-15 13:35:05 +0800

    Add trailler button

--- src/components/NowShowing.jsx
+++ src/components/NowShowing.jsx
@@ -2,7 +2,9 @@ import moment from "moment";
 import { useGetNowPlayingQuery } from "../services/api";
 import { useRef } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { BsPlayFill } from "react-icons/bs";
 import Section from "./Section/Section";
+import { Link } from "react-router-dom";
 
 const NowShowing = () => {
   const scrollRef = useRef(null);
@@ -56,44 +58,56 @@ const NowShowing = () => {
         {nowPlaying?.results?.map((playing, index) => (
           <div
             key={playing.id}
-            className={`${index === 0 ? "md:row-span-2" : ""} relative`}
+            className={`${index === 0 ? "md:row-span-2" : ""} relative group`}
           >
-            <div
-              className={`${
-                index === 0 ? "w-full md:w-[450px]" : "w-72 sm:w-96"
-              } h-full overflow-hidden rounded-md`}
-            >
-              <img
-                src={`https://image.tmdb.org/t/p/w500${playing.backdrop_path}`}
-                alt=""
-                className={`object-cover ${
-                  index === 0 ? "h-full" : "h-[270px]"
-                }`}
-              />
-            </div>
-            <div className="absolute bottom-10 z-10 px-5">
-              <div className="flex items-center gap-4">
-                {index === 0 && (
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500${playing.poster_path}`}
-                    className="h-56 object-contain rounded-md hidden md:block"
-                  />
-                )}
-                <div>
-                  <h2 className="text-lg sm:text-2xl font-bold">
-                    {playing?.title}
-                  </h2>
-                  <span className="text-gray-300">
-                    {playing.original_language}
-                  </span>{" "}
-                  •{" "}
-                  <span className="text-sm text-gray-300">
-                    {moment(playing?.release_date).format("MMMM D YYYY")}
-                  </span>
+            <Link to={`/movie/details/${playing.id}`}>
+              <div
+                className={`${
+                  index === 0 ? "w-full md:w-[450px]" : "w-72 sm:w-96"
+                } h-full overflow-hidden rounded-md`}
+              >
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${playing.backdrop_path}`}
+                  alt=""
+                  className={`object-cover ${
+                    index === 0 ? "h-full" : "h-[270px]"
+                  } group-hover:scale-110 overflow-hidden transition-transform duration-300`}
+                />
+              </div>
+              <div className="absolute bottom-10 z-10 px-5">
+                <div className="flex items-center gap-4">
+                  {index === 0 && (
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500${playing.poster_path}`}
+                      className="h-56 object-contain rounded-md hidden md:block"
+                    />
+                  )}
+                  <div className="flex flex-col">
+                    <div>
+                      <h2 className="text-lg sm:text-2xl font-bold">
+                        {playing?.title}
+                      </h2>
+                      <span className="text-gray-300">
+                        {playing.original_language}
+                      </span>{" "}
+                      •{" "}
+                      <span className="text-sm text-gray-300">
+                        {moment(playing?.release_date).format("MMMM D YYYY")}
+                      </span>
+                    </div>
+                  </div>
                 </div>
               </div>
+              <div className="absolute top-0 w-full h-full bg-black/40 rounded-md overflow-hidden" />
+            </Link>
+            <div className="absolute bottom-20 z-10 right-40">
+              {index === 0 && (
+                <button className="bg-background-dark flex items-center justify-center gap-1 rounded-full py-1 px-4 mt-2.5">
+                  <BsPlayFill className="text-2xl" />
+                  <span>Trailler</span>
+                </button>
+              )}
             </div>
-            <div className="absolute top-0 w-full h-full bg-black/40 rounded-md overflow-hidden" />
           </div>
         ))}
       </div>
