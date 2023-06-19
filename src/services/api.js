03afa113aa3dce01ef60bd9a0dacacc9b56f74c6
rosanno <rosannomacabuhay@gmail.com>
2023-06-19 10:02:23 +0800

    Added popular people endpoints

--- src/services/api.js
+++ src/services/api.js
@@ -11,10 +11,7 @@ export const tmdbApi = createApi({
      * Discover Movies
      */
     getDiscover: builder.query({
-      query: (type) =>
-        `/3/discover/${
-          type === "movies" ? "movie" : "tv"
-        }?api_key=${api_key}&language=en-US&page=1`,
+      query: (type) => `/3/discover/${type === "movies" ? "movie" : "tv"}?api_key=${api_key}&language=en-US&page=1`,
       keepUnusedDataFor: 5,
     }),
     /**
@@ -30,34 +27,23 @@ export const tmdbApi = createApi({
      * Get Movies Videos
      */
     getVideo: builder.query({
-      query: ({ type, id }) =>
-        `/3/${
-          type === "movie" ? "movie" : "tv"
-        }/${id}/videos?api_key=${api_key}&language=en-US`,
+      query: ({ type, id }) => `/3/${type === "movie" ? "movie" : "tv"}/${id}/videos?api_key=${api_key}&language=en-US`,
     }),
     /**
      * Get Movies Logo
      */
     getLogo: builder.query({
-      query: ({ type, id }) =>
-        `/3/${
-          type === "movie" ? "movie" : "tv"
-        }/${id}/images?api_key=${api_key}`,
+      query: ({ type, id }) => `/3/${type === "movie" ? "movie" : "tv"}/${id}/images?api_key=${api_key}`,
     }),
     getRuntime: builder.query({
-      query: ({ type, id }) =>
-        `/3/${
-          type === "movie" ? "movie" : "tv"
-        }/${id}?api_key=${api_key}&language=en-US`,
+      query: ({ type, id }) => `/3/${type === "movie" ? "movie" : "tv"}/${id}?api_key=${api_key}&language=en-US`,
     }),
     /**
      * Popular movies
      */
     getPopular: builder.query({
       query: ({ type, page }) =>
-        `/3/${
-          type === "movies" ? "movie" : "tv"
-        }/popular?api_key=${api_key}&language=en-US&page=${page}`,
+        `/3/${type === "movies" ? "movie" : "tv"}/popular?api_key=${api_key}&language=en-US&page=${page}`,
       serializeQueryArgs: ({ endpoint }) => {
         return endpoint;
       },
@@ -78,9 +64,7 @@ export const tmdbApi = createApi({
      */
     getNowPlaying: builder.query({
       query: ({ type }) =>
-        `/3/${
-          type === "movies" ? "movie" : "tv"
-        }/now_playing?api_key=${api_key}&language=en-US&page=1`,
+        `/3/${type === "movies" ? "movie" : "tv"}/now_playing?api_key=${api_key}&language=en-US&page=1`,
     }),
     /**
      * Upcoming movies
@@ -92,8 +76,7 @@ export const tmdbApi = createApi({
      * Get single movies details
      */
     getMovieDetails: builder.query({
-      query: (movie_id) =>
-        `/3/movie/${movie_id}?api_key=${api_key}&language=en-US`,
+      query: (movie_id) => `/3/movie/${movie_id}?api_key=${api_key}&language=en-US`,
     }),
     /**
      * Popular tv series
@@ -102,10 +85,13 @@ export const tmdbApi = createApi({
       query: () => `/3/tv/popular?api_key=${api_key}&language=en-US&page=1`,
     }),
     getMovieGenre: builder.query({
-      query: ({ type }) =>
-        `/3/genre/${
-          type === "movies" ? "movie" : "tv"
-        }/list?api_key=${api_key}&language=en`,
+      query: ({ type }) => `/3/genre/${type === "movies" ? "movie" : "tv"}/list?api_key=${api_key}&language=en`,
+    }),
+    /**
+     * Popular People
+     */
+    getPopularPeople: builder.query({
+      query: () => `/3/person/popular?api_key=${api_key}&language=en-US&page=1`,
     }),
   }),
 });
@@ -122,4 +108,5 @@ export const {
   useGetPopularQuery,
   useGetPopularSeriesQuery,
   useGetMovieGenreQuery,
+  useGetPopularPeopleQuery,
 } = tmdbApi;
