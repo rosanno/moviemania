4d2b6e57dabcf470a6ec87d805e428c4cf2da163
rosanno <rosannomacabuhay@gmail.com>
2023-06-19 19:30:35 +0800

    Fixed no title display in TV Shows

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -10,12 +10,8 @@ const Popular = ({ movie }) => {
         className="rounded-md overflow-hidden"
       />
       <div>
-        <h4 className="text-sm mt-2 font-semibold truncate overflow-hidden">
-          {movie?.title}
-        </h4>
-        <p className="text-sm text-gray-400 mt-1">
-          {moment(movie?.release_date).format("MMMM D YYYY")}
-        </p>
+        <h4 className="text-sm mt-2 font-semibold truncate overflow-hidden">{movie?.title || movie?.name}</h4>
+        <p className="text-sm text-gray-400 mt-1">{moment(movie?.release_date).format("MMMM D YYYY")}</p>
       </div>
     </Link>
   );
