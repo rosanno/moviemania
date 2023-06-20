659964ab83147262e5b1b0c31d7197f0e71156ae
rosanno <rosannomacabuhay@gmail.com>
2023-06-20 14:38:43 +0800

    Added language in moviecard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,9 @@
+import langToLang from "language-name-to-language-name";
 import moment from "moment";
 import { Link } from "react-router-dom";
 
 const MovieCard = ({ media }) => {
+  const language = langToLang(media.original_language);
   return (
     <Link to={`/movies/details/${media.id}`} key={media.id}>
       <div className="relative group overflow-hidden">
@@ -18,8 +20,10 @@ const MovieCard = ({ media }) => {
         </div>
         <div className="absolute bottom-10 z-10 px-5">
           <h2 className="text-lg sm:text-2xl font-bold">{media?.title || media?.name}</h2>
-          <span className="text-gray-300">{media.original_language}</span> •{" "}
-          <span className="text-sm text-gray-300">{moment(media?.release_date).format("MMMM D YYYY")}</span>
+          <span className="text-gray-300 capitalize text-sm">
+            {language?.en.name ? language?.en.name : "English"}
+          </span>{" "}
+          • <span className="text-sm text-gray-300">{moment(media?.release_date).format("MMMM D YYYY")}</span>
         </div>
         <div className="absolute top-0 w-full h-full bg-black/30 rounded-md overflow-hidden" />
       </div>
