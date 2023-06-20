76c8ff1acfc34f48f483f71b06556d3816e6c8a2
rosanno <rosannomacabuhay@gmail.com>
2023-06-20 15:34:43 +0800

    Fixed no logo return of some movie

--- src/hooks/useLogoFilter.js
+++ src/hooks/useLogoFilter.js
@@ -4,7 +4,7 @@ export const useLogoFilter = (data) => {
   const [logo, setLogo] = useState();
 
   useEffect(() => {
-    const logo = data?.logos?.find((logo) => logo.iso_639_1 === "en");
+    const logo = data?.logos?.find((logo) => logo.iso_639_1 === "en" || logo.iso_639_1 === null);
     setLogo(logo);
   }, [data]);
 
