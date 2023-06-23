c182e112b174d5650a3fadbffb2723a388c38945
rosanno <rosannomacabuhay@gmail.com>
2023-06-23 15:58:49 +0800

    Added nav active in mobile nav

--- src/components/NavMobile.jsx
+++ src/components/NavMobile.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { AiOutlineClose } from "react-icons/ai";
 
 const NavMobile = ({ isMobileNavOpen, setIsMobileNavOpen }) => {
+  const { pathname } = useLocation();
+
   return (
     <div
       className={`bg-background-dark fixed z-50 h-screen inset-0 ${
-        !isMobileNavOpen
-          ? "-translate-y-full transition-transform"
-          : "translate-y-0"
+        !isMobileNavOpen ? "-translate-y-full transition-transform" : "translate-y-0"
       } duration-300`}
     >
       <button onClick={() => setIsMobileNavOpen(false)}>
@@ -15,16 +15,16 @@ const NavMobile = ({ isMobileNavOpen, setIsMobileNavOpen }) => {
       </button>
       <nav className="py-32 px-10">
         <ul>
-          <li onClick={() => setIsMobileNavOpen(false)} className="nav__links">
+          <li className={`nav__links ${pathname === "/" && "text-yellow-500"}`}>
             <Link to="/">Home</Link>
           </li>
-          <li onClick={() => setIsMobileNavOpen(false)} className="nav__links">
-            <Link to="/popular-movies">Popular Movies</Link>
+          <li className={`nav__links ${pathname === "/popular-movies" && "text-yellow-500"}`}>
+            <Link to="/popular-movies">Movies</Link>
           </li>
-          <li onClick={() => setIsMobileNavOpen(false)} className="nav__links">
+          <li className={`nav__links ${pathname === "/tv-shows" && "text-yellow-500"}`}>
             <Link to="/tv-shows">TV Shows</Link>
           </li>
-          <li onClick={() => setIsMobileNavOpen(false)} className="nav__links">
+          <li className={`nav__links ${pathname === "/popular-people" && "text-yellow-500"}`}>
             <Link to="/popular-people">Popular people</Link>
           </li>
         </ul>
