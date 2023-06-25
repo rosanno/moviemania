d5f41296da669b34185b388fc7b39ee9f7095183
rosanno <rosannomacabuhay@gmail.com>
2023-06-25 14:21:23 +0800

    Adjusted image height on small devices

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -9,10 +9,10 @@ const Popular = ({ movie }) => {
         <img
           src={`https://image.tmdb.org/t/p/w500${movie?.poster_path}`}
           alt={movie?.title}
-          className="w-full md:max-w-[200px] h-[250px] md:max-h-[250px] rounded-md overflow-hidden"
+          className="w-full md:max-w-[200px] h-[280px] md:max-h-[250px] rounded-md overflow-hidden"
         />
       ) : (
-        <div className="w-full md:max-w-[200px] h-[250px] md:max-h-[250px] bg-gray-300 rounded-md flex items-center justify-center">
+        <div className="w-full md:max-w-[200px] h-[280px] md:max-h-[250px] bg-gray-300 rounded-md flex items-center justify-center">
           <IoImageOutline className="text-gray-400 text-2xl" />
         </div>
       )}
