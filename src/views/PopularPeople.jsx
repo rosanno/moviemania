1349207008a50c4b7dd41cc935a80c026d9a0b14
rosanno <rosannomacabuhay@gmail.com>
2023-06-25 20:25:40 +0800

    Added Loader

--- src/views/PopularPeople.jsx
+++ src/views/PopularPeople.jsx
@@ -5,6 +5,7 @@ import People from "../components/People";
 import Content from "../components/content/Content";
 import { useGetPopularPeopleQuery } from "../services/api";
 import Button from "../components/Button/Button";
+import Loader from "../components/Loader/Loader";
 
 const PopularPeople = () => {
   const [page, setPage] = useState(1);
@@ -39,21 +40,19 @@ const PopularPeople = () => {
     setPage((prev) => prev + 1);
   };
 
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <>
       <Content variant="secondary">
         <div className="mt-16 sm:mt-20 md:mt-32 px-4 sm:px-6">
           <h1 className="text-xl sm:text-2xl font-medium capitalize mb-1 sm:mb-4">Popular People</h1>
           <Grid>
-            {isLoading ? (
-              <h1>Loading</h1>
-            ) : (
-              <>
-                {people?.results?.map((movie, index) => (
-                  <People key={index} movie={movie} />
-                ))}
-              </>
-            )}
+            {people?.results?.map((movie, index) => (
+              <People key={index} movie={movie} />
+            ))}
           </Grid>
           {!loadMore && (
             <div className="flex justify-center">
