bbc4678dc753f5f37b7762636ed3043c921d5715
rosanno <rosannomacabuhay@gmail.com>
2023-06-25 14:13:52 +0800

    Fixed image grid height

--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -4,7 +4,7 @@ const Grid = ({ children, variant, gap }) => {
   }
 
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">{children}</div>
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">{children}</div>
   );
 };
 
--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,18 +1,21 @@
 import { Link } from "react-router-dom";
 import moment from "moment";
+import { IoImageOutline } from "react-icons/io5";
 
 const Popular = ({ movie }) => {
   return (
     <Link to={`/movie/details/${movie?.id}`} className="mt-4">
-      <img
-        src={`${
-          movie?.poster_path !== null
-            ? `https://image.tmdb.org/t/p/w500${movie?.poster_path}`
-            : "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg"
-        } `}
-        alt={movie?.title}
-        className="rounded-md overflow-hidden"
-      />
+      {movie?.poster_path !== null ? (
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie?.poster_path}`}
+          alt={movie?.title}
+          className="w-full md:max-w-[200px] h-[290px] md:max-h-[250px] rounded-md overflow-hidden"
+        />
+      ) : (
+        <div className="w-full md:max-w-[200px] h-[250px] bg-gray-300 rounded-md flex items-center justify-center">
+          <IoImageOutline className="text-gray-400 text-2xl" />
+        </div>
+      )}
       <div>
         <h4 className="text-sm mt-2 font-semibold truncate overflow-hidden">
           {movie?.title ? movie?.title : movie?.name}
