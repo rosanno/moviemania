44031b3939625ff9277ee69bf6e0712f3360b990
rosanno <rosannomacabuhay@gmail.com>
2023-06-25 21:36:41 +0800

    Fix image height not align properly

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -1,19 +1,25 @@
+import { Link } from "react-router-dom";
+import { IoImageOutline } from "react-icons/io5";
+
 const Popular = ({ movie }) => {
-  const mapJoin = movie?.known_for.map((item) => item.original_title).join(", ");
+  const known_for = movie?.known_for.map((item) => item.original_title || item.name).join(", ");
 
   return (
-    <div>
+    <Link to={`/person/${movie?.id}`} className="mt-2">
       <img
-        src={`https://image.tmdb.org/t/p/w500${movie?.profile_path}`}
+        src={`${
+          movie?.profile_path
+            ? `https://image.tmdb.org/t/p/w500${movie?.profile_path}`
+            : "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg"
+        } `}
         alt={movie?.title}
-        className="rounded-md overflow-hidden"
+        className="h-[240px] sm:h-full sm:max-h-[280px] w-full object-cover rounded-md overflow-hidden"
       />
-      <div className="overflow-hidden w-48 truncate">
+      <div className="overflow-hidden sm:w-48 truncate">
         <h4 className="text-sm mt-2 font-semibold truncate ">{movie?.name}</h4>
-
-        <span className="text-sm text-gray-400 mt-1  ">{mapJoin}</span>
+        <span className="text-sm text-gray-400 mt-1">{known_for}</span>
       </div>
-    </div>
+    </Link>
   );
 };
 
