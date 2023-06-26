cccf36ce9ece85abd5403380b5fe79f7b19304e7
rosanno <rosannomacabuhay@gmail.com>
2023-06-26 15:15:42 +0800

    Completed people details pages

--- src/views/Details/Details.jsx
+++ src/views/Details/Details.jsx
@@ -1,11 +1,19 @@
 import { Link, useLocation, useParams } from "react-router-dom";
+import { motion } from "framer-motion";
+
 import Content from "../../components/content/Content";
-import { useGetCreditsQuery, useGetMediaDetailsQuery, useGetRecommendationQuery } from "../../services/api";
+import {
+  useGetCreditsQuery,
+  useGetMediaDetailsQuery,
+  useGetRecommendationQuery,
+  useGetSimilarQuery,
+} from "../../services/api";
 import Loader from "../../components/Loader/Loader";
 import Credit from "../../components/Credit";
 import Hero from "../../components/Hero";
 import Showcase from "../../components/Showcase";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Modal from "../../components/Modal";
 
 const Details = () => {
   const { pathname } = useLocation();
@@ -14,12 +22,14 @@ const Details = () => {
   const { data: media, isLoading } = useGetMediaDetailsQuery({ type, id });
   const { data: credits } = useGetCreditsQuery({ type, id });
   const { data: recommendations } = useGetRecommendationQuery({ id, type });
+  const { data: similar } = useGetSimilarQuery({ id, type });
+  const [openModal, setModalOpen] = useState(false);
 
   // const genre = media?.genres?.map((genre) => genre.name).join(", ");
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+  }, [pathname]);
 
   if (isLoading) {
     return <Loader />;
@@ -27,9 +37,21 @@ const Details = () => {
 
   return (
     <>
+      {openModal && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            duration: 0.5,
+          }}
+          onClick={() => setModalOpen(false)}
+          className="fixed z-40 inset-0 overflow-hidden bg-gradient-to-r from-black/80 backdrop-blur"
+        />
+      )}
+      {openModal && <Modal media={media} setModalOpen={setModalOpen} openModal={openModal} isVideo={type} />}
       <Hero media={media} />
       <Content variant="primary">
-        <Showcase media={media} isMediaSelected={false} media_type={type} />
+        <Showcase media={media} isMediaSelected={false} media_type={type} setModalOpen={setModalOpen} />
       </Content>
       <Content isSpacerOnly>
         <section className="relative z-20 px-3 sm:px-6 transition-all duration-1000 ease-in mt-20 custom-container">
@@ -46,14 +68,37 @@ const Details = () => {
         </section>
         <section className="relative z-20 px-3 sm:px-6 transition-all duration-1000 ease-in mt-20 custom-container">
           <div className="border-t border-gray-400/40 mb-10" />
+          <h3 className="text-3xl font-semibold text-gray-300">
+            Similar <span className="capitalize">{type === "movie" ? "Movies" : "TV Show"}</span>{" "}
+          </h3>
+          {similar?.results?.length !== 0 ? (
+            <div className="grid grid-flow-col auto-cols-max gap-2 overflow-x-auto scrollbar mt-5">
+              {similar?.results?.map((item) => (
+                <Link to={`/${type}/details/${item.id}`} key={item.id}>
+                  <Credit {...item} />
+                </Link>
+              ))}
+            </div>
+          ) : (
+            <h1 className="text-sm text-gray-300 pt-10 text-center">
+              No Recommendation for this <span className="capitalize">{type === "tv" && `${type} Show`}</span>
+            </h1>
+          )}
+        </section>
+        <section className="relative z-20 px-3 sm:px-6 transition-all duration-1000 ease-in mt-20 custom-container">
+          <div className="border-t border-gray-400/40 mb-10" />
           <h3 className="text-3xl font-semibold text-gray-300">Recommendations</h3>
-          <div className="grid grid-flow-col auto-cols-max gap-2 overflow-x-auto scrollbar mt-5">
-            {recommendations?.results?.map((recommend) => (
-              <Link to={`/${recommend.media_type}/details/${recommend.id}`} key={recommend.id}>
-                <Credit {...recommend} />
-              </Link>
-            ))}
-          </div>
+          {recommendations?.results?.length !== 0 ? (
+            <div className="grid grid-flow-col auto-cols-max gap-2 overflow-x-auto scrollbar mt-5">
+              {recommendations?.results?.map((recommend) => (
+                <Link to={`/${recommend.media_type}/details/${recommend.id}`} key={recommend.id}>
+                  <Credit {...recommend} />
+                </Link>
+              ))}
+            </div>
+          ) : (
+            <h1 className="text-sm text-gray-300 pt-10 text-center">No Recommendation for this {type}</h1>
+          )}
         </section>
       </Content>
     </>
--- src/views/Person/PersonDetails.jsx
+++ src/views/Person/PersonDetails.jsx
@@ -1,13 +1,23 @@
 import React, { useEffect, useState } from "react";
+import { AiOutlineInstagram, AiOutlineYoutube } from "react-icons/ai";
+import { BsTiktok } from "react-icons/bs";
+import { CiFacebook } from "react-icons/ci";
+import { FiTwitter } from "react-icons/fi";
+
 import { Link, useParams } from "react-router-dom";
 import { BiChevronRight } from "react-icons/bi";
 import { IoImageOutline } from "react-icons/io5";
-import { useGetCreditMoviesQuery, useGetPersonDetailsQuery } from "../../services/api";
+import {
+  useGetCreditMoviesQuery,
+  useGetExternalIDQuery,
+  useGetPersonDetailsQuery,
+  useGetProfileQuery,
+} from "../../services/api";
 import Content from "../../components/content/Content";
 import Credit from "../../components/Credit";
 import Loader from "../../components/Loader/Loader";
 
-const Details = ({ heading, details, isRepeated = false }) => {
+const Details = ({ heading, details, isRepeated = false, age }) => {
   if (isRepeated) {
     return (
       <div className="mt-3">
@@ -24,8 +34,18 @@ const Details = ({ heading, details, isRepeated = false }) => {
   return (
     <div className="mt-3">
       <h3 className="text-base text-gray-300">{heading}</h3>
-      <p className="text-sm font-light text-gray-300">{details}</p>
+      <p className="text-sm font-light text-gray-300">
+        {details} <span>{age ? `${age} years old` : ""}</span>
+      </p>
     </div>
+  );
+};
+
+const SocialAccounts = ({ url, social_id, icon }) => {
+  return (
+    <Link to={`${url}${social_id}`} target="_blank" className="text-2xl inline-block pt-7">
+      {icon}
+    </Link>
   );
 };
 
@@ -33,6 +53,8 @@ const PersonDetails = () => {
   const { id } = useParams();
   const { data: details, isLoading } = useGetPersonDetailsQuery({ id });
   const { data: creditMovie } = useGetCreditMoviesQuery({ personId: id });
+  const { data: profiles } = useGetProfileQuery({ id });
+  const { data: externalIds } = useGetExternalIDQuery({ id });
   const [bioLength, setBioLength] = useState(450);
 
   const sortedCreditMovie = creditMovie?.cast && [...creditMovie.cast]?.sort((a, b) => b.vote_count - a.vote_count);
@@ -61,25 +83,56 @@ const PersonDetails = () => {
                 <IoImageOutline className="text-gray-400 text-2xl" />
               </div>
             )}
-            <div className="mt-5 sm:mt-16">
+            <div className="flex items-center gap-2">
+              {externalIds?.facebook_id && (
+                <SocialAccounts
+                  url="https://www.facebook.com/"
+                  social_id={externalIds?.facebook_id}
+                  icon={<CiFacebook />}
+                />
+              )}
+              {externalIds?.instagram_id && (
+                <SocialAccounts
+                  url="https://www.instagram.com/"
+                  social_id={externalIds?.instagram_id}
+                  icon={<AiOutlineInstagram />}
+                />
+              )}
+              {externalIds?.twitter_id && (
+                <SocialAccounts
+                  url="https://www.twitter.com/"
+                  social_id={externalIds?.twitter_id}
+                  icon={<FiTwitter />}
+                />
+              )}
+              {externalIds?.tiktok_id && (
+                <SocialAccounts url="https://www.tiktok.com/" social_id={externalIds?.tiktok_id} icon={<BsTiktok />} />
+              )}
+              {externalIds?.youtube_id && (
+                <SocialAccounts
+                  url="https://www.tiktok.com/"
+                  social_id={externalIds?.youtube_id}
+                  icon={<AiOutlineYoutube />}
+                />
+              )}
+            </div>
+            <div className="mt-5 sm:mt-7">
               <h2 className="text-xl text-gray-300">Personal Info</h2>
               <div className="grid grid-cols-2 md:grid-cols-1">
                 <Details heading="Known for" details={details?.known_for_department} />
                 <Details heading="Known Credits" details={creditMovie?.cast?.length} />
                 <Details heading="Gender" details={details?.gender === 2 ? "Male" : "Female"} />
-                <div className="mt-3">
-                  <h3 className="text-base text-gray-300">Birthday</h3>
-                  <p className="text-sm text-gray-300">
-                    {details?.birthday} (<span>{new Date().getFullYear() - parseInt(details?.birthday)} years old</span>
-                    )
-                  </p>
-                </div>
+                <Details
+                  heading="Birthday"
+                  details={details?.birthday}
+                  age={new Date().getFullYear() - parseInt(details?.birthday)}
+                />
                 <Details heading="Place of Birth" details={details?.place_of_birth} />
                 <Details heading="Alson Known As" details={details} isRepeated />
               </div>
             </div>
           </div>
-          <div className="sm:w-[350px] md:w-[640px] xl:flex-1">
+          <div className="w-full md:w-[640px] xl:flex-1">
             <h1 className="text-4xl font-bold">{details?.name}</h1>
             <div className="mt-5">
               <h3 className="text-lg font-semibold">Biography</h3>
@@ -103,7 +156,7 @@ const PersonDetails = () => {
                 </>
               )}
             </div>
-            <div className="mt-10">
+            <div className="mt-10 max-h-[240px]">
               <h3 className="text-lg font-semibold">Known For</h3>
               <div className="w-[870px] mt-1">
                 <div className="grid grid-flow-col auto-cols-max gap-2 overflow-x-auto scrollbar">
@@ -118,9 +171,23 @@ const PersonDetails = () => {
                 </div>
               </div>
             </div>
-            <div className="h-[740px] border border-dashed flex flex-col items-center justify-center rounded-md">
-              <img src="/construction.gif" alt="" className="h-[230px]" />
-              Under construction
+            <div className="mt-14">
+              <h2 className="text-3xl text-gray-300 font-semibold pb-4">Profiles</h2>
+              {profiles?.profiles?.length !== 0 ? (
+                <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+                  {profiles?.profiles?.map((profile, index) => (
+                    <div key={index}>
+                      <img
+                        src={`https://www.themoviedb.org/t/p/w300${profile.file_path}`}
+                        alt=""
+                        className="rounded-md"
+                      />
+                    </div>
+                  ))}
+                </div>
+              ) : (
+                <h1>No Profiles Added</h1>
+              )}
             </div>
           </div>
         </div>
