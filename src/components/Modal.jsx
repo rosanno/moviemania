3e88ce1d1cb3c2727297079b445b78682626597c
rosanno <rosannomacabuhay@gmail.com>
2023-06-27 13:29:03 +0800

    Adjusted  modal height on medium devices

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -47,7 +47,7 @@ const Modal = ({ media, setModalOpen, openModal, isVideo }) => {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-background-dark text-left shadow-xl transition-all sm:my-8 w-full h-[250px] sm:w-full sm:max-w-[1085px] sm:h-[650px]">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-background-dark text-left shadow-xl transition-all sm:my-8 w-full h-[360px] sm:w-full sm:max-w-[1085px] md:h-[580px]">
                 <div className="bg-background-dark px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div>
                     <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
@@ -64,7 +64,7 @@ const Modal = ({ media, setModalOpen, openModal, isVideo }) => {
                                 ref={videoRef}
                                 src={`https://www.youtube.com/embed/${video?.key}/?autoplay=1&mute=1&loop=1&controls=0`}
                                 title="Playback"
-                                className="w-full h-[210px] sm:w-[520px] md:w-[930px] md:h-[600px] scale-150 object-cover brightness-110"
+                                className="w-full h-[350px] sm:w-[520px] md:w-[930px] md:h-[600px] scale-150 object-cover brightness-110"
                               ></iframe>
                             ) : (
                               <div className="flex justify-center items-center h-96">
