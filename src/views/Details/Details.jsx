8231e35c3f3056fcf723f61c2dfc638368559ade
rosanno <rosannomacabuhay@gmail.com>
2023-06-27 17:10:02 +0800

    Minor changes

--- src/views/Details/Details.jsx
+++ src/views/Details/Details.jsx
@@ -81,7 +81,7 @@ const Details = () => {
             </div>
           ) : (
             <h1 className="text-sm text-gray-300 pt-10 text-center">
-              No Recommendation for this <span className="capitalize">{type === "tv" && `${type} Show`}</span>
+              No Similar for this <span className="capitalize">{type === "tv" ? `${type} Show` : type}</span>
             </h1>
           )}
         </section>
@@ -97,7 +97,7 @@ const Details = () => {
               ))}
             </div>
           ) : (
-            <h1 className="text-sm text-gray-300 pt-10 text-center">No Recommendation for this {type}</h1>
+            <h1 className="text-sm text-gray-300 pt-10 text-center capitalize">No Recommendation for this {type}</h1>
           )}
         </section>
       </Content>
