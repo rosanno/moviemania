e82fa12ea85abd289e609e56a6ab5a6502254230
rosanno <rosannomacabuhay@gmail.com>
2023-06-28 14:25:51 +0800

    updated

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,3 @@
-import { AnimatePresence } from "framer-motion";
 import langToLang from "language-name-to-language-name";
 import moment from "moment";
 import { useState } from "react";
@@ -25,54 +24,50 @@ const MovieCard = ({ media, isVideo }) => {
     <>
       <BackdropBlur onClose={onClose} isOpen={openModal} />
       <Modal media={media} setModalOpen={setModalOpen} openModal={openModal} isVideo={isVideo} />
-      <div className="w-full h-full">
-        <div className="relative group w-72 sm:w-96 overflow-hidden cursor-default">
-          <div className="rounded-md overflow-hidden">
-            <img
-              src={`${
-                media?.backdrop_path
-                  ? `https://image.tmdb.org/t/p/w500${media?.backdrop_path}`
-                  : "https://upload.wikimedia.org/wikipedia/commons/7/75/No_image_available.png"
-              }`}
-              alt=""
-              className="h-[270px] object-cover"
-            />
-            <div className="absolute bottom-10 z-20 px-5 group-hover:-translate-y-32 transition-transform duration-300 ">
-              <h2 className="text-lg sm:text-2xl group-hover:text-base transition duration-300 font-bold">
-                {media?.title || media?.name}
-              </h2>
-              <span className="text-gray-300 capitalize text-sm">
-                {language?.en.name ? language?.en.name : "English"}
-              </span>{" "}
-              • <span className="text-sm text-gray-300">{moment(media?.release_date).format("MMMM D YYYY")}</span>
-            </div>
-            <div className="absolute z-10 inset-0 group-hover:-translate-y-0 translate-y-full w-full h-full transition duration-300 bg-black/30 backdrop-blur rounded-md overflow-hidden">
-              <div className="translate-y-28 px-5 py-1">
-                <h4 className="font-bold text-sm">Overview</h4>
-              </div>
-              <div className="h-[60px] overflow-y-auto scrollbar px-5 translate-y-28">
-                <p className="text-xs sm:text-sm">{media?.overview}</p>
-              </div>
-              <div className="px-5 translate-y-28 flex gap-3 mt-4">
-                <button
-                  onClick={() => setModalOpen(true)}
-                  className="flex items-center gap-1 text-sm hover:bg-[#FFAE06] bg-[#FFC54E] transition duration-300 rounded-full px-5 py-1"
-                >
-                  <FaPlay size="15px" />
-                  <span>Trailler</span>
-                </button>
-                <button
-                  onClick={() => handleRouteChange(media.id)}
-                  className="flex items-center gap-1 text-sm hover:bg-[#FFC54E] bg-[#FFAE06] transition duration-300 rounded-full px-5 py-1"
-                >
-                  <LuClipboardList size="20px" />
-                  <span>Details</span>
-                </button>
-              </div>
-            </div>
-            <div className="absolute top-0 w-full h-full bg-black/30 rounded-md overflow-hidden" />
+      <div className="relative group w-72 sm:w-96 h-[270px] overflow-hidden cursor-default">
+        <div className="rounded-md overflow-hidden">
+          <img
+            src={`${
+              media?.backdrop_path
+                ? `https://image.tmdb.org/t/p/w500${media?.backdrop_path}`
+                : "https://upload.wikimedia.org/wikipedia/commons/7/75/No_image_available.png"
+            }`}
+            alt=""
+            className="h-[270px] object-cover"
+          />
+        </div>
+        <div className="absolute bottom-10 z-20 px-5 group-hover:-translate-y-32 transition-transform duration-300 ">
+          <h2 className="text-lg sm:text-2xl group-hover:text-base transition duration-300 font-bold">
+            {media?.title || media?.name}
+          </h2>
+          <span className="text-gray-300 capitalize text-sm">{language?.en.name ? language?.en.name : "English"}</span>{" "}
+          • <span className="text-sm text-gray-300">{moment(media?.release_date).format("MMMM D YYYY")}</span>
+        </div>
+        <div className="absolute bottom-0 left-0 right-0 z-10 group-hover:h-full h-0 transition-all duration-300 bg-black/30 backdrop-blur rounded-md overflow-hidden">
+          <div className="translate-y-28 px-5 py-1">
+            <h4 className="font-bold text-sm">Overview</h4>
+          </div>
+          <div className="h-[60px] overflow-y-auto scrollbar px-5 translate-y-28">
+            <p className="text-xs sm:text-sm">{media?.overview}</p>
+          </div>
+          <div className="px-5 translate-y-28 flex gap-3 mt-4">
+            <button
+              onClick={() => setModalOpen(true)}
+              className="flex items-center gap-1 text-sm hover:bg-[#FFAE06] bg-[#FFC54E] transition duration-300 rounded-full px-5 py-1"
+            >
+              <FaPlay size="15px" />
+              <span>Trailler</span>
+            </button>
+            <button
+              onClick={() => handleRouteChange(media.id)}
+              className="flex items-center gap-1 text-sm hover:bg-[#FFC54E] bg-[#FFAE06] transition duration-300 rounded-full px-5 py-1"
+            >
+              <LuClipboardList size="20px" />
+              <span>Details</span>
+            </button>
           </div>
         </div>
+        <div className="absolute top-0 w-full h-full bg-black/30 rounded-md overflow-hidden" />
       </div>
     </>
   );
