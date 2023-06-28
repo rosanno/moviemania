b19f0c65ffcd83059c706dd189da1322057963c8
rosanno <rosannomacabuhay@gmail.com>
2023-06-28 17:58:54 +0800

    Loader added while searching

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,10 +3,11 @@ import { HiOutlineMagnifyingGlass } from "react-icons/hi2";
 import { useGetSearchQuery } from "../services/api";
 import { Link } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
+import { Oval } from "react-loader-spinner";
 
 const Search = ({ isSearch, query, setQuery, onClose }) => {
   const inputRef = useRef(null);
-  const { data: searchResults } = useGetSearchQuery({ query }, { skip: !isSearch });
+  const { data: searchResults, isFetching } = useGetSearchQuery({ query }, { skip: !isSearch });
 
   useEffect(() => {
     inputRef.current.focus();
@@ -39,34 +40,53 @@ const Search = ({ isSearch, query, setQuery, onClose }) => {
             </div>
             <div className="absolute top-16 flex flex-col items-center w-full px-2">
               {query !== "" && (
-                <div className="bg-black/50 backdrop-blur px-6 w-full sm:w-[530px] md:w-[640px] rounded-md max-h-[340px] overflow-y-auto scrollbar">
-                  {searchResults?.results?.map((item) => (
-                    <Link
-                      onClick={onClose}
-                      to={`/${item.media_type}${item.media_type === "person" ? "" : "/details"}/${item.id}`}
-                      key={item.id}
-                      className="my-2 block"
-                    >
-                      <div className="flex gap-2">
-                        <img
-                          src={`${
-                            item?.poster_path !== null && item?.profile_path !== null
-                              ? `https://image.tmdb.org/t/p/w300${item?.poster_path || item?.profile_path}`
-                              : "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg"
-                          } `}
-                          alt=""
-                          className="w-20"
-                        />
-                        <div className="w-[510px]">
-                          <h3>{item.title || item.original_name}</h3>
-                          <p className="text-xs pt-1 text-gray-400 capitalize">
-                            <span>{item.media_type}</span>
-                          </p>
-                          <p className="text-xs pt-2 text-gray-400 truncate overflow-hidden">{item.overview}</p>
-                        </div>
-                      </div>
-                    </Link>
-                  ))}
+                <div className="bg-black/50 backdrop-blur px-6 w-full sm:w-[530px] md:w-[640px] rounded-md h-auto max-h-[340px] overflow-y-auto scrollbar">
+                  {isFetching ? (
+                    <div className="flex justify-center py-4">
+                      <Oval
+                        height={30}
+                        width={30}
+                        color="#404144"
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                        visible={true}
+                        ariaLabel="oval-loading"
+                        secondaryColor="#404144"
+                        strokeWidth={2}
+                        strokeWidthSecondary={2}
+                      />
+                    </div>
+                  ) : (
+                    <>
+                      {searchResults?.results?.map((item) => (
+                        <Link
+                          onClick={onClose}
+                          to={`/${item.media_type}${item.media_type === "person" ? "" : "/details"}/${item.id}`}
+                          key={item.id}
+                          className="my-2 block"
+                        >
+                          <div className="flex gap-2">
+                            <img
+                              src={`${
+                                item?.poster_path !== null && item?.profile_path !== null
+                                  ? `https://image.tmdb.org/t/p/w300${item?.poster_path || item?.profile_path}`
+                                  : "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg"
+                              } `}
+                              alt=""
+                              className="w-20"
+                            />
+                            <div className="w-[510px]">
+                              <h3>{item.title || item.original_name}</h3>
+                              <p className="text-xs pt-1 text-gray-400 capitalize">
+                                <span>{item.media_type}</span>
+                              </p>
+                              <p className="text-xs pt-2 text-gray-400 truncate overflow-hidden">{item.overview}</p>
+                            </div>
+                          </div>
+                        </Link>
+                      ))}
+                    </>
+                  )}
                 </div>
               )}
             </div>
