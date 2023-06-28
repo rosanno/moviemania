1b516ff522afc462b15e4c61402c5bbf84b56f6e
rosanno <rosannomacabuhay@gmail.com>
2023-06-28 08:35:02 +0800

    reduce heading font size on small devices

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -4,8 +4,8 @@ import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 const Section = ({ children, heading, scrollLeft, scrollRight }) => {
   return (
     <section className="md:pl-6 px-2 md:pt-14 xl:ml-7 2xl:ml-28">
-      <div className="flex justify-between items-center mb-5 mt-10">
-        <h1 className="text-2xl sm:text-3xl font-bold capitalize">{heading}</h1>
+      <div className="flex justify-between items-center mb-3 sm:mb-5 mt-10">
+        <h1 className="text-xl sm:text-3xl font-bold capitalize">{heading}</h1>
         <div className="hidden sm:flex items-center justify-center gap-2 md:mr-4 2xl:mr-32">
           <div
             onClick={scrollLeft}
