e54ba39d3aec09f42cf2394d84f9d308a848e429
rosanno <rosannomacabuhay@gmail.com>
2023-06-30 13:21:37 +0800

    replace loader animation with backdrop blur

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -10,31 +10,17 @@ const Popular = ({ movie, media_type }) => {
   return (
     <Link to={`/${media_type}/details/${movie?.id}`} className="mt-4">
       {movie?.poster_path !== null ? (
-        <div className="relative">
-          {!imageLoaded && (
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2">
-              <Oval
-                height={40}
-                width={40}
-                color="#404144"
-                wrapperStyle={{}}
-                wrapperClass=""
-                visible={true}
-                ariaLabel="oval-loading"
-                secondaryColor="#404144"
-                strokeWidth={4}
-                strokeWidthSecondary={4}
-              />
-            </div>
-          )}
+        <>
           <img
             src={`https://image.tmdb.org/t/p/w500${movie?.poster_path}`}
             alt={movie?.title}
             loading="lazy"
             onLoad={() => setImageLoaded(true)}
-            className="w-full md:max-w-[200px] h-[260px] md:max-h-[250px] rounded-md overflow-hidden"
+            className={`${
+              !imageLoaded && "filter blur-sm"
+            } w-full md:max-w-[200px] h-[260px] md:max-h-[250px] rounded-md overflow-hidden`}
           />
-        </div>
+        </>
       ) : (
         <div className="w-full md:max-w-[200px] h-[260px] md:max-h-[250px] bg-gray-300 rounded-md flex items-center justify-center">
           <IoImageOutline className="text-gray-400 text-2xl" />
