2a4a2f5fa55a0a2c5d365a7f9b3c768d151f87fe
rosanno <rosannomacabuhay@gmail.com>
2023-06-30 10:04:09 +0800

    added preloading on images

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,19 +1,42 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
 import { IoImageOutline } from "react-icons/io5";
+import { Oval } from "react-loader-spinner";
 
 const Popular = ({ movie, media_type }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return (
     <Link to={`/${media_type}/details/${movie?.id}`} className="mt-4">
       {movie?.poster_path !== null ? (
-        <img
-          src={`https://image.tmdb.org/t/p/w500${movie?.poster_path}`}
-          alt={movie?.title}
-          loading="lazy"
-          className="w-full md:max-w-[200px] h-[280px] md:max-h-[250px] rounded-md overflow-hidden"
-        />
+        <div className="relative">
+          {!imageLoaded && (
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2">
+              <Oval
+                height={40}
+                width={40}
+                color="#404144"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+                ariaLabel="oval-loading"
+                secondaryColor="#404144"
+                strokeWidth={4}
+                strokeWidthSecondary={4}
+              />
+            </div>
+          )}
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie?.poster_path}`}
+            alt={movie?.title}
+            loading="lazy"
+            onLoad={() => setImageLoaded(true)}
+            className="w-full md:max-w-[200px] h-[260px] md:max-h-[250px] rounded-md overflow-hidden"
+          />
+        </div>
       ) : (
-        <div className="w-full md:max-w-[200px] h-[280px] md:max-h-[250px] bg-gray-300 rounded-md flex items-center justify-center">
+        <div className="w-full md:max-w-[200px] h-[260px] md:max-h-[250px] bg-gray-300 rounded-md flex items-center justify-center">
           <IoImageOutline className="text-gray-400 text-2xl" />
         </div>
       )}
