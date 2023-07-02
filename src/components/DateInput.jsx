587e1a159f88e4c2d5785536dadde0d383f1aaca
rosanno <rosannomacabuhay@gmail.com>
2023-07-02 14:44:58 +0800

    minor changes

--- src/components/DateInput.jsx
+++ src/components/DateInput.jsx
@@ -2,7 +2,11 @@ const DateInput = ({ label, setDate }) => {
   return (
     <div className="flex items-center justify-between gap-4">
       <label className="text-sm">{label}</label>
-      <input type="date" className="text-sm px-4 py-1 my-1 rounded-md" onChange={(e) => setDate(e.target.value)} />
+      <input
+        type="date"
+        className="text-sm px-2 py-1.5 my-1 rounded-md"
+        onChange={(e) => setDate(e.target.value)}
+      />
     </div>
   );
 };
