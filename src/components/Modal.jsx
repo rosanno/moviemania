ee4bc3f6da928e679c611524e1c46f432a5b49bb
rosanno <rosannomacabuhay@gmail.com>
2023-07-04 11:08:12 +0800

    reduce iframe scale

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -25,7 +25,12 @@ const Modal = ({ media, setModalOpen, openModal, isVideo }) => {
 
   return (
     <Transition.Root show={openModal} as={Fragment}>
-      <Dialog as="div" className="relative z-50" initialFocus={cancelButtonRef} onClose={setModalOpen}>
+      <Dialog
+        as="div"
+        className="relative z-50"
+        initialFocus={cancelButtonRef}
+        onClose={setModalOpen}
+      >
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -39,7 +44,7 @@ const Modal = ({ media, setModalOpen, openModal, isVideo }) => {
         </Transition.Child>
 
         <div className="fixed inset-0 z-50 overflow-y-auto">
-          <div className="flex sm:min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-4">
+          <div className="flex sm:min-h-full items-end justify-center px-1.5 py-4 text-center sm:items-center sm:p-4">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -49,15 +54,10 @@ const Modal = ({ media, setModalOpen, openModal, isVideo }) => {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-background-dark text-left shadow-xl transition-all sm:my-8 w-full h-[320px] sm:w-full sm:max-w-[1085px] md:h-[580px]">
-                <div className="bg-background-dark px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-background-dark text-left shadow-xl transition-all sm:my-8 w-full h-[310px] sm:h-[560px] sm:w-[900px] xl:w-full xl:max-w-[970px] md:h-[608px]">
+                <div className="bg-background-dark">
                   <div>
-                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                      <Dialog.Title as="h3" className="text-base font-semibold leading-6">
-                        <span className="absolute left-2 top-2 z-20 hidden sm:block sm:text-sm">
-                          {media?.title || media?.name}
-                        </span>
-                      </Dialog.Title>
+                    <div className="mt-3 text-center sm:mt-0 sm:text-left">
                       <div className="mt-2">
                         {openModal && (
                           <>
@@ -66,15 +66,17 @@ const Modal = ({ media, setModalOpen, openModal, isVideo }) => {
                                 {video !== undefined ? (
                                   <iframe
                                     ref={videoRef}
-                                    src={`https://www.youtube.com/embed/${video?.key}/?autoplay=1&mute=1&loop=1&controls=0`}
+                                    src={`https://www.youtube.com/embed/${video?.key}`}
                                     title="Playback"
-                                    className="w-full h-[300px] sm:w-[520px] md:w-[930px] md:h-[600px] scale-150 object-cover brightness-110"
+                                    className="w-full h-[300px] sm:h-[550px] md:w-full xl:md:w-[970px] md:h-[600px] object-cover brightness-110"
                                   ></iframe>
                                 ) : (
                                   <div className="flex justify-center items-center sm:h-[480px]">
                                     <div className="flex flex-col items-center gap-2">
                                       <BsCameraVideoOff className="text-2xl text-gray-400" />
-                                      <h1 className="text-xl text-gray-400 font-semibold">No video</h1>
+                                      <h1 className="text-xl text-gray-400 font-semibold">
+                                        No video
+                                      </h1>
                                     </div>
                                   </div>
                                 )}
